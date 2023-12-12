d51faa5c3a93714d9abaa4533a91a52f0883f45c
yeetpan <tiwariswayam88@gmail.com>
2023-12-12 14:33:24 +0530

    minor changes

--- server.js
+++ server.js
@@ -342,4 +342,7 @@ app.get('/dashboard', (req, res) => {
 
 
 // Start the server
-app.listen(process.env.PORT);
+const PORT = process.env.PORT|| 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
