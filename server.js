138fee4c32c9ce39dc7c96b57a2afbedd9df99ff
sai1099 <chsaimurali09@gmail.com>
2023-12-30 12:10:13 +0530

    commit-no..

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID:  process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: 'https://nebula-1zdu.onrender.com/auth/google/callback'
   },
   (accessToken, refreshToken, profile, done) => {
 
